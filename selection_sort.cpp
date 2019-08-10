0dbf3bd7885b6149325f002182e58550f8b7d69d
Hitesh Sharma <hitesharma3899@gmail.com>
2019-08-10 11:49:13 +0530

    Selection Sort

---
+++ selection_sort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+void selection(int *a, int n){
+  int i,j,m,x,t,f;
+  for(i=0;i<n-1;i++){
+    m=a[i];
+    f=0;
+    for(j=i+1;j<n;j++){
+      if(a[j]<m){
+        m=a[j];
+        x=j;
+        f=1;
+      }
+    }
+    if(f==1){
+    t=a[i];
+    a[i]=a[x];
+    a[x]=t;
+  }
+  }
+  for(i=0;i<n;i++){
+    cout<<a[i]<<" ";
+  }
+  cout<<endl;
+}
+int main(int argc, const char* argv[]){
+  int i,a[argc-1],r=3,c=3;
+  for(i=0;i<argc-1;i++){
+    a[i]=atoi(argv[i+1]);
+  }
+  selection(a,argc-1);
+}
